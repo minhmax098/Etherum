45c85ce515d1c5acb1321578494a76029a008e3e
minhmax098 <hocminh1998@gmail.com>
2025-11-05 11:55:39 +0300

    commit solidity example

--- contracts/onek.sol
+++ contracts/onek.sol
@@ -5,12 +5,14 @@ contract BasitContract {
     address public enYuksekTeklifiVeren;
     bool public mezatSonlandi;
     address public mezatSahibi;
+    uint public bitis;
 
-    function Sahip(address _mezatSahibi) external  {
-        mezatSahibi = _mezatSahibi;
-    }
+    // function Sahip(address _mezatSahibi) external  {
+    //     mezatSahibi = _mezatSahibi;
+    // }
 
-    constructor(address _mezatSahibi) {
+    constructor(address _mezatSahibi, uint _sure) {
         mezatSahibi = _mezatSahibi;
+        bitis = block.timestamp + _sure * 1 minutes;
     }
 }
