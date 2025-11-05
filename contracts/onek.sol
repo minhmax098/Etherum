33dbcccc10c867728b51fdd049074bbf7f9e7f41
minhmax098 <hocminh1998@gmail.com>
2025-11-05 11:39:13 +0300

    commit solidity sample

--- contracts/onek.sol
+++ contracts/onek.sol
@@ -1,9 +1,16 @@
 pragma solidity ^0.8.30;
 
 contract BasitContract {
-    uint enYuksekTeklif = 100;
-    address enYuksekTeklifiVeren;
-    bool mezatSonlandi;
+    uint public enYuksekTeklif = 100;
+    address public enYuksekTeklifiVeren;
+    bool public mezatSonlandi;
+    address public mezatSahibi;
 
-    
+    function Sahip(address _mezatSahibi) external  {
+        mezatSahibi = _mezatSahibi;
+    }
+
+    constructor(address _mezatSahibi) {
+        mezatSahibi = _mezatSahibi;
+    }
 }
